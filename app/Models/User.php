a8b6a7578ca6f154ef4905cea81bc61814ce5c9f
Nick C <me@nickc.io>
2015-08-12 17:36:04 -0400

    Added helper methods for registering/reseting user account data

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Bican\Roles\Traits\HasRoleAndPermission;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Support\Str;
 
 class User extends Model implements HasRoleAndPermissionContract, AuthenticatableContract
 {
@@ -32,4 +33,42 @@ class User extends Model implements HasRoleAndPermissionContract, Authenticatabl
      * @var array
      */
     protected $hidden = ['password'];
+
+    /**
+     * Generate a create user token and set it to the correct field
+     *
+     * @param string $field
+     * @return bool
+     */
+    public function generateToken($field)
+    {
+        $fieldName = $field . 'Token';
+
+        $this->attributes[$fieldName] = Str::random(64);
+        return $this->save();
+    }
+
+    /**
+     * User verify email - set account as active
+     *
+     * @return void
+     */
+    public function setActive()
+    {
+        $this->attributes['createToken'] = null;
+        $this->attributes['verified'] = 1;
+
+        $this->save();
+    }
+
+    /**
+     * Mutator to hash password
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
