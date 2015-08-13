9f9baa42644c37f7bcef59572ba31159561fdc52
Nick C <me@nickc.io>
2015-08-13 18:16:29 -0400

    adjust base request should validate logic

---
+++ tests/Unit/Requests/RequestTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit\Requests;
+
+use Mockery as m;
+
+class RequestTest extends \TestCase
+{
+    public function setUp()
+    {
+        $this->request = new TestRequest();
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+    
+    public function testShouldValidateChecksForValueInValidateMethodsArray()
+    {
+        $method = $this->getProtectedMethod('shouldValidate');
+
+        $this->assertFalse($method->invokeArgs($this->request, ['get']));
+        $this->assertTrue($method->invokeArgs($this->request, ['post']));
+        $this->assertFalse($method->invokeArgs($this->request, ['delete']));
+        $this->assertTrue($method->invokeArgs($this->request, ['put']));
+        $this->assertFalse($method->invokeArgs($this->request, ['pew']));
+    }
+
+    public function testHasAltRulesReturnsTrueWhenHasRules()
+    {
+        $method = $this->getProtectedMethod('hasAltRules');
+
+        $this->assertTrue($method->invokeArgs($this->request, ['get']));
+        $this->assertTrue($method->invokeArgs($this->request, ['post']));
+        $this->assertTrue($method->invokeArgs($this->request, ['put']));
+        $this->assertTrue($method->invokeArgs($this->request, ['delete']));
+    }
+
+    public function testHasAltRulesReturnsFalseWhenNoRules()
+    {
+        $method = $this->getProtectedMethod('hasAltRules');
+
+        $this->assertFalse($method->invokeArgs($this->request, ['patch']));
+        $this->assertFalse($method->invokeArgs($this->request, ['options']));
+        $this->assertFalse($method->invokeArgs($this->request, ['something']));
+        $this->assertFalse($method->invokeArgs($this->request, ['pewpew']));
+    }
+
+    public function testGetAltRulesReturnsMatching()
+    {
+        $method = $this->getProtectedMethod('getAltRules');
+        $expected = ['test' => 'rules', 'get' => 'rules'];
+        $rules = $method->invokeArgs($this->request, ['get']);
+
+        $this->assertEquals($expected, $rules);
+
+    }
+
+    protected function getProtectedMethod($methodName)
+    {
+        $class = new \ReflectionClass($this->request);
+        $method = $class->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+}
