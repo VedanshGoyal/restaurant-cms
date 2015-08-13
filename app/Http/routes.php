28e9d202f90eec6f7c12284ac19c5791c25e1568
Nick C <me@nickc.io>
2015-08-13 19:08:17 -0400

    update routes to use middleware and add api prefix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,79 +1,90 @@
 <?php
 
+// HTML routes
 Route::get('/', 'HomeController@index');
 
-// Auth Routes
+// API routes
+Route::group(['prefix' => 'api-V1'], function () {
 
-Route::post('/login', 'AuthController@login');
-Route::post('/register', 'AuthController@register');
-Route::post('/reset-password', 'AuthController@resetPassword');
-Route::get(
-    '/verify-new/{token}',
-    [
-        'uses' => 'AuthController@verifyNew',
-        'as' => 'verify-new',
-    ]
-);
-Route::get(
-    '/verify-reset/{token}',
-    [
-        'uses' => 'AuthController@verifyReset',
-        'as' => 'verify-reset',
-    ]
-);
+    // Auth routes
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/reset-password', 'AuthController@resetPassword');
+    Route::get(
+        '/verify-new/{token}',
+        [
+            'uses' => 'AuthController@verifyNew',
+            'as' => 'verify-new',
+        ]
+    );
+    Route::get(
+        '/verify-reset/{token}',
+        [
+            'uses' => 'AuthController@verifyReset',
+            'as' => 'verify-reset',
+        ]
+    );
 
-// API Routes
+    // Admin only routes
+    Route::group(['middleware' => 'checkRole:admin'], function () {
 
-Route::resource(
-    'menu-section',
-    'MenuSectionsController',
-    ['only' => ['index', 'store', 'show', 'update', 'destroy']]
-);
+        Route::resource(
+            'site-config',
+            'SiteConfigController',
+            ['only' => ['show', 'update']]
+        );
 
-Route::resource(
-    'menu-item',
-    'MenuItemsController',
-    ['only' => ['store', 'show', 'update', 'destroy']]
-);
+        Route::resource(
+            'role',
+            'RolesController',
+            ['only' => ['index']]
+        );
 
-Route::resource(
-    'about',
-    'AboutController',
-    ['only' => ['show', 'update']]
-);
+        Route::resource(
+            'user',
+            'UsersController',
+            ['only' => ['index']]
+        );
+    });
 
-Route::resource(
-    'info',
-    'InfoController',
-    ['only' => ['show', 'update']]
-);
+    // Owner only routes
+    Route::group(['middleware' => 'checkRole:owner'], function () {
+        Route::resource(
+            'menu-section',
+            'MenuSectionsController',
+            ['only' => ['index', 'store', 'show', 'update', 'destroy']]
+        );
 
-Route::resource(
-    'hour',
-    'HoursController',
-    ['only' => ['index', 'show', 'update']]
-);
+        Route::resource(
+            'menu-item',
+            'MenuItemsController',
+            ['only' => ['store', 'show', 'update', 'destroy']]
+        );
 
-Route::resource(
-    'photo',
-    'PhotosController',
-    ['only' => ['index', 'show', 'store', 'destroy']]
-);
+        Route::resource(
+            'about',
+            'AboutController',
+            ['only' => ['show', 'update']]
+        );
 
-Route::resource(
-    'site-config',
-    'SiteConfigController',
-    ['only' => ['show', 'update']]
-);
+        Route::resource(
+            'info',
+            'InfoController',
+            ['only' => ['show', 'update']]
+        );
 
-Route::resource(
-    'role',
-    'RolesController',
-    ['only' => ['index']]
-);
+        Route::resource(
+            'hour',
+            'HoursController',
+            ['only' => ['index', 'show', 'update']]
+        );
 
-Route::resource(
-    'user',
-    'UsersController',
-    ['only' => ['index']]
-);
+        Route::resource(
+            'photo',
+            'PhotosController',
+            ['only' => ['index', 'show', 'store', 'destroy']]
+        );
+
+    });
+
+});
