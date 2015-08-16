0a7570733778524b55aa220296f52333aefb0656
Nick C <me@nickc.io>
2015-08-16 17:19:33 -0400

    update password regex wording

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -76,7 +76,7 @@ return [
     'url'                  => 'The :attribute format is invalid.',
     'basicText'            => 'The :attribute field may only be letters, numbers, spaces, and punctuation.',
     'tags'                 => 'Tags should only be letters, numbers, spaces, and punctuation.',
-    'password'             => 'Password can be 8-64 characters in length, and may only contain letters, numbers and !@#$%.',
+    'password'             => 'Password can be 8-64 characters in length, can only be letters, numbers, !@#$% and must contain at least one number.',
 
     /*
     |--------------------------------------------------------------------------
