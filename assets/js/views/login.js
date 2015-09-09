93369523dc05c11de5b5c8885fb6469f53d298ea
Nick C <me@nickc.io>
2015-09-08 22:15:03 -0400

    add login view form submit logic

--- assets/js/views/login.js
+++ assets/js/views/login.js
@@ -1,12 +1,35 @@
 import {ItemView, TemplateCache} from 'backbone.marionette';
+import Config from '../config';
 import MDLBehavior from '../behaviors/mdl';
+import FormBehavior from '../behaviors/form';
+import ModelErrorBehavior from '../behaviors/model-error';
+import NotifyService from '../services/notify';
 
 export default ItemView.extend({
     template: TemplateCache.get('login'),
     tagName: 'div',
     className: 'mdl-card mdl-cell mdl-cell--8-col mdl-shadow--2dp',
+    modelUrl: 'login',
+
+    events: {
+        'submit form': 'handleSubmit',
+    },
 
     behaviors: {
         mdl: {behaviorClass: MDLBehavior},
+        form: {behaviorClass: FormBehavior},
+        modelError: {behaviorClass: ModelErrorBehavior},
+    },
+
+    handleSubmit() {
+        this.model.url = Config.apiRoot + '/login';
+        this.model.set(this.form);
+
+        this.model.save().done(response => {
+            NotifyService.request('success', 'Welcome back!');
+            let attributesString = JSON.stringify(this.model.attributes);
+            window.localStorage.setItem(Config.storageName, attributesString);
+            window.location.hash = ' ';
+        });
     },
 });
