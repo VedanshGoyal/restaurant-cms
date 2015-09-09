980f796623f6fbfc22b0f3192a920e8deaa1a7d6
Nick C <me@nickc.io>
2015-09-08 21:34:15 -0400

    add basic model error behavior

---
+++ assets/js/behaviors/model-error.js
@@ -0,0 +1,29 @@
+import _ from 'underscore';
+import {Behavior} from 'backbone.marionette';
+import NotifyService from '../services/notify';
+
+export default Behavior.extend({
+    modelEvents: {
+        'error': 'handleError',
+    },
+
+    handleError(model, xhr) {
+        let status = xhr.status;
+        let errors = JSON.parse(xhr.responseText);
+
+        if (status === 422) {
+            this._markInputs(_.keys(errors));
+        }
+
+        if (_.isObject(errors)) {
+            NotifyService.request('error', errors);
+        }
+    },
+
+    _markInputs(fields) {
+        _.each(fields, field => {
+            let $el = this.view.$(`input[name=${field}]`).parent('div');
+            $el.addClass('is-invalid');
+        });
+    },
+});
