9a29c56937ecaba56709d907cb3adafc065efc99
Nick C <me@nickc.io>
2015-09-09 18:30:14 -0400

    setup password reset logic

--- assets/js/views/forgot.js
+++ assets/js/views/forgot.js
@@ -1,12 +1,35 @@
 import {ItemView, TemplateCache} from 'backbone.marionette';
+import Config from '../config';
 import MDLBehavior from '../behaviors/mdl';
+import FormBehavior from '../behaviors/form';
+import ModelErrorBehavior from '../behaviors/model-error';
+import NotifyService from '../services/notify';
+import LoadingService from '../services/loading';
 
 export default ItemView.extend({
     template: TemplateCache.get('forgot'),
     tagName: 'div',
     className: 'mdl-card mdl-cell mdl-cell--8-col mdl-shadow--2dp',
 
+    events: {
+        'submit form': 'handleSubmit',
+    },
+
     behaviors: {
         mdl: {behaviorClass: MDLBehavior},
+        form: {behaviorClass: FormBehavior},
+        modelError: {behaviorClass: ModelErrorBehavior},
+    },
+
+    handleSubmit() {
+        LoadingService.request('show');
+
+        this.model.url = Config.apiRoot + '/reset-password';
+        this.model.set(this.form).save().done(response => {
+            LoadingService.request('hide');
+            NotifyService.request('success', 'Please check your email for a password reset link.');
+
+            window.location.hash = 'login';
+        });
     },
 });
