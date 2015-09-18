f36f87d7dbdc7103c99711035062a74d3e54c3a8
Nick C <me@nickc.io>
2015-09-18 18:07:45 -0400

    Update router service usage to work with new service classes

--- assets/js/routers/auth.js
+++ assets/js/routers/auth.js
@@ -1,4 +1,5 @@
 import {Router} from 'backbone.routing';
+import AuthSerivce from '../services/auth';
 import NavbarService from '../services/navbar';
 import HeaderService from '../services/header';
 import Route from '../routes/route';
@@ -17,42 +18,42 @@ export default Router.extend({
         'verify-new/:token': 'verifyNew',
     },
 
-    initialize(options = {}) {
-        this.model = options.model;
+    initialize() {
+        this.model = AuthSerivce.model;
     },
 
     login() {
-        NavbarService.request('setAuthActive', 'login');
-        HeaderService.request('setTitle', 'Login');
+        NavbarService.setAuthActive('login');
+        HeaderService.setTitle('Login');
 
         return new Route(new LoginView({model: this.model}));
     },
 
     create() {
-        NavbarService.request('setAuthActive', 'create');
-        HeaderService.request('setTitle', 'New Account');
+        NavbarService.setAuthActive('create');
+        HeaderService.setTitle('Create New Account');
     
         return new Route(new CreatView({model: this.model}));
     },
 
     forgot() {
-        NavbarService.request('setAuthActive', 'forgot');
-        HeaderService.request('setTitle', 'Forgot Password');
+        NavbarService.setAuthActive('forgot');
+        HeaderService.setTitle('Forgot Password');
 
         return new Route(new ForgotView({model: this.model}));
     },
 
     verifyReset(token) {
-        NavbarService.request('setAuthActive');
-        HeaderService.request('setTitle', 'Reset Password');
+        NavbarService.setAuthActive();
+        HeaderService.setTitle('Reset Password');
         this.model.set('verify-token', token);
 
         return new Route(new VerifyResetView({model: this.model}));
     },
 
     verifyNew(token) {
-        NavbarService.request('setAuthActive');
-        HeaderService.request('setTitle', 'Verify New Account');
+        NavbarService.setAuthActive();
+        HeaderService.setTitle('Verify New Account');
         this.model.set('verify-token', token);
 
         return new Route(new VerifyNewView({model: this.model}));
--- assets/js/routers/index.js
+++ assets/js/routers/index.js
@@ -1,24 +1,15 @@
 import {Router} from 'backbone.routing';
 import NavbarService from '../services/navbar';
-import AuthService from '../services/auth';
 import ProtectedRoute from '../routes/protected-route';
 import HomeView from '../views/home';
 
 export default Router.extend({
     routes: {
         '': 'index',
-        'home': 'home',
     },
 
     index() {
-        AuthService.request('isAuthed').then(isAuthed => {
-            let nextHash = (isAuthed) ? 'home' : 'login';
-            location.hash = nextHash;
-        });
-    },
-
-    home() {
-        NavbarService.request('setContentActive', 'home');
+        NavbarService.setContentActive('home');
 
         return new ProtectedRoute(new HomeView());
     },
