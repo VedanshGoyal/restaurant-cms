d99a39e335810977b957530c47f2a2a532b1a85a
Nick C <me@nickc.io>
2015-09-17 22:35:29 -0400

    remove unused layout regions

--- assets/js/layouts/app.js
+++ assets/js/layouts/app.js
@@ -4,8 +4,6 @@ export default LayoutView.extend({
     el: '.application',
     template: TemplateCache.get('app'),
     regions: {
-        header: '.header',
-        nav: '.navigation',
         content: '.content',
     },
 });
