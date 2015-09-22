37bdc3bce19eba45efddd352f763b1f85f234566
Nick C <me@nickc.io>
2015-09-22 15:49:36 -0400

    add info display view

---
+++ assets/js/views/info/show.js
@@ -0,0 +1,21 @@
+import {ItemView, TemplateCache} from 'backbone.marionette';
+import LoadingService from '../../services/loading';
+import MDLBehavior from '../../behaviors/mdl';
+
+export default ItemView.extend({
+    template: TemplateCache.get('infoShow'),
+    className: 'mdl-card mdl-cell mdl-cell--8-col mdl-shadow--2dp',
+    tagName: 'div',
+
+    behaviors: {
+        mdl: {behaviorClass: MDLBehavior},
+    },
+
+    modelEvents: {
+        sync: 'render',
+    },
+
+    initialize() {
+        this.model.fetch();
+    },
+});
