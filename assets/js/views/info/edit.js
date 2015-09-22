f5dfd7b521b57eec5a2228c3ca5b8033b91f63e2
Nick C <me@nickc.io>
2015-09-22 15:49:49 -0400

    add info edit view

---
+++ assets/js/views/info/edit.js
@@ -0,0 +1,41 @@
+import {ItemView, TemplateCache} from 'backbone.marionette';
+import LoadingService from '../../services/loading';
+import NotifyService from '../../services/notify';
+import MDLBehavior from '../../behaviors/mdl';
+import FormBehavior from '../../behaviors/form';
+import ModelErrorBehavior from '../../behaviors/model-error';
+
+export default ItemView.extend({
+    template: TemplateCache.get('infoEdit'),    
+    className: 'mdl-card mdl-cell mdl-cell--8-col mdl-shadow--2dp',
+    tagName: 'div',
+
+    events: {
+        'submit form': 'handleSubmit',
+    },
+
+    behaviors: {
+        mdl: {behaviorClass: MDLBehavior},
+        form: {behaviorClass: FormBehavior},
+        modelError: {behaviorClass: ModelErrorBehavior},
+    },
+
+    modelEvents: {
+        sync: 'render',
+    },
+
+    initialize() {
+        this.model.fetch();
+    },
+
+    handleSubmit() {
+        LoadingService.show();
+
+        this.model.set(this.form).save().done(() => {
+            LoadingService.hide();
+            NotifyService.success('Info updated successfully.');
+
+            window.location.hash = '#info';
+        });
+    },
+});
