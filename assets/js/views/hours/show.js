621cee43d7f8453f5857fd5294e45a12b92d66f0
Nick C <me@nickc.io>
2015-09-23 22:46:41 -0400

    add hour show view

---
+++ assets/js/views/hours/show.js
@@ -0,0 +1,37 @@
+import {ItemView, CompositeView, TemplateCache} from 'backbone.marionette';
+
+const HourView = ItemView.extend({
+    template: TemplateCache.get('hourSingle'),
+    tagName: 'tr',
+
+    templateHelpers() {
+        return {
+            closedStatus() {
+                return (this.isClosed) ? 'Yes' : 'No';
+            },
+        };
+    },
+});
+
+export default CompositeView.extend({
+    childView: HourView,
+    template: TemplateCache.get('hourWrapper'),
+    className: 'mdl-data-table mdl-js-data-table mdl-cell mdl-cell--8-col mdl-shadow--2dp',
+    tagName: 'table',
+
+    ui: {
+        wrapper: 'tbody',
+    },
+
+    collectionEvents: {
+        sync: 'render',
+    },
+
+    initialize() {
+        this.collection.fetch();
+    },
+
+    attachBuffer(view, buffer) {
+        view.ui.wrapper.html(buffer);
+    },
+});
