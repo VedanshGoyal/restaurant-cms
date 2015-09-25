2cd5ead3258ecde5ad3e294ee9dbeafee16d97cc
Nick C <me@nickc.io>
2015-09-24 20:57:08 -0400

    fix tablet and footer spacing on frontpage template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,11 +24,13 @@
                     </nav>
                 </div>
             </header>
+
             <div class="mdl-layout__drawer">
                 <nav class="mdl-navigation">
                     <a class="mdl-navigation__link" href="">Link</a>
                 </nav>
             </div>
+
             <main class="mdl-layout__content">
 
                 <section class="section--center mdl-grid mdl-grid--no-spacing info-section text-center">
@@ -37,14 +39,14 @@
                     </div>
                 </section>
 
-                <section class="mdl-grid title-section mdl-shadow--4dp">
+                <section class="mdl-grid title-section mdl-shadow--2dp">
                     <div class="mdl-cell mdl-cell--12-col text-center">
                         <h1>{{{ $info->name }}}</h1>
                     </div>
                 </section>
 
-                <section class="section--center mdl-grid mdl-grid--no-spacing about-section">
-                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center mdl-shadow--4dp">
+                <section class="section--center mdl-grid">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center mdl-shadow--2dp">
                         <div class="mdl-card__title mdl-card--border">
                             <h2 class="mdl-card__title-text">{{{ $about->title }}}</h2>
                         </div>
@@ -54,8 +56,8 @@
                     </div>
                 </section>
 
-                <section class="section--center mdl-grid mdl-grid--no-spacing">
-                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center mdl-shadow--4dp menu-section">
+                <section class="section--center mdl-grid">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center mdl-shadow--2dp">
                         <div class="mdl-card__title mdl-card--border">
                             <h2 class="mdl-card__title-text">Menu</h2>
                         </div>
@@ -64,7 +66,7 @@
                             <h5>{{{ $menuSection->name }}}</h5>
                             <div class="mdl-grid">
                             @foreach($menuSection->items as $item)
-                                <div class="section__text mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet md-cell-12--col-phone">
+                                <div class="section__text mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet md-cell-4--col-phone">
                                     <h6>{{{ $item->name }}}</h6>
                                     @if(isset($item->description))
                                     <p>{{{ $item->description }}}</p>
@@ -84,25 +86,25 @@
                     </div>
                 </section>
 
-                <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--4dp">
-                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center">
+                <section class="section--center mdl-grid">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col text-center mdl-shadow--2dp">
                         <div class="mdl-card__title mdl-card--border">
                             <h2 class="mdl-card__title-text">Photos</h2>
                         </div>
                         <div class="mdl-card__supporting-text mdl-grid">
-                            <div class="mdl-card mdl-card__image mdl-shadow--4dp mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone" style="background: url('/imgs/cityscape1.jpg') center / cover;">
+                            <div class="mdl-card mdl-card__image mdl-shadow--2dp mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone" style="background: url('/imgs/cityscape1.jpg') center / cover;">
                                 <div class="mdl-card__title mdl-card--expand"></div>
                                 <div class="mdl-card__actions">
                                     <span>Image.jpg</span>
                                 </div>
                             </div>
-                            <div class="mdl-card mdl-card__image mdl-shadow--4dp mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--hide-phone" style="background: url('/imgs/cityscape1.jpg') center / cover;">
+                            <div class="mdl-card mdl-card__image mdl-shadow--2dp mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--hide-phone" style="background: url('/imgs/cityscape1.jpg') center / cover;">
                                 <div class="mdl-card__title mdl-card--expand"></div>
                                 <div class="mdl-card__actions">
                                     <span>Image.jpg</span>
                                 </div>
                             </div>
-                            <div class="mdl-card mdl-card__image mdl-shadow--4dp mdl-cell--4-col-desktop mdl-cell--hide-phone mdl-cell--hide-tablet" style="background: url('/imgs/cityscape1.jpg') center / cover;">
+                            <div class="mdl-card mdl-card__image mdl-shadow--2dp mdl-cell--4-col-desktop mdl-cell--hide-phone mdl-cell--hide-tablet" style="background: url('/imgs/cityscape1.jpg') center / cover;">
                                 <div class="mdl-card__title mdl-card--expand"></div>
                                 <div class="mdl-card__actions">
                                     <span>Image.jpg</span>
@@ -115,9 +117,10 @@
                         </div>
                     </div>
                 </section><!-- .photo-section -->
+
                 <footer class="mdl-mega-footer">
                     <div class="mdl-mega-footer__top-section mlg-grid text-center">
-                        <div class="mdl-mega-footer__left-section mdl-cell--6-col mdl-cell--12-tablet">
+                        <div class="mdl-mega-footer__left-section mdl-cell--6-col mdl-cell--4-col-tablet">
                             <h6 class="mdl-mega-footer__heading">Restaurant CMS</h6>
                             <ul class="mdl-mega-footer__link-list">
                                 <li>{{{ $info->name }}}</li>
@@ -130,7 +133,7 @@
                             </ul>
                         </div>
 
-                        <div class="mdl-mega-footer__left-section mdl-cell--6-col">
+                        <div class="mdl-mega-footer__left-section mdl-cell--6-col mdl-cell--4-col-tablet">
                             <h6 class="mdl-mega-footer__heading">Hours</h6>
                             <ul class="mdl-mega-footer__link-list">
                                 @foreach($hours as $hour)
@@ -139,7 +142,6 @@
                             </ul>
                         </div>
                     </div>
-
                     <div class="mdl-mega-footer__bottom-section mdl-nospacing">
                         <div class="mdl-logo">Made with &#9829 by NC</div>
                         <ul class="mdl-mega-footer__link-list mdl-nospacing">
@@ -147,9 +149,9 @@
                         </ul>
                     </div>
                 </footer>
+
             </main>
         </div>
-
         <script type="text/javascript" src="/build/frontpage.js"></script>
     </body>
 </html>
