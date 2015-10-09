aa339cf3b6e5205e3b1746a823e240f9eb8c1e92
Nick C <me@nickc.io>
2015-10-09 18:41:10 -0400

    add edit hour view, submit functionality not complete

---
+++ assets/js/views/hours/edit.js
@@ -0,0 +1,30 @@
+import {ItemView, TemplateCache} from 'backbone.marionette';
+import LoadingService from '../../services/loading';
+import NotifyService from '../../services/notify';
+import MDLBehavior from '../../behaviors/mdl';
+import FormBehavior from '../../behaviors/form';
+import ModelErrorBehavior from '../../behaviors/model-error';
+
+export default ItemView.extend({
+    template: TemplateCache.get('hourEdit'),
+    className: 'mdl-card mdl-cell mdl-cell--8-col mdl-shadow--2dp',
+    tagName: 'div',
+
+    events: {
+        'form submit': 'handleSubmit',
+    },
+
+    modelEvents: {
+        sync: 'render',
+    },
+
+    initialize() {
+        this.model.fetch();
+    },
+
+    behaviors: {
+        mdl: {behaviorClass: MDLBehavior},
+        form: {behaviorClass: FormBehavior},
+        modelError: {behaviorClass: ModelErrorBehavior},
+    },
+});
