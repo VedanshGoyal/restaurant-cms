1345b45b6b6e52089b814a7cc5dfe552a55d5e48
Nick C <me@nickc.io>
2015-10-09 11:39:24 -0400

    Initialize new hour model if not in hours collection

--- assets/js/routers/hour.js
+++ assets/js/routers/hour.js
@@ -3,6 +3,7 @@ import NavbarService from '../services/navbar';
 import HeaderService from '../services/header';
 import ProtectedRoute from '../routes/protected-route';
 import HoursCollection from '../collections/hours';
+import HourModel from '../models/hour';
 import ShowView from '../views/hours/show';
 import EditView from '../views/hours/edit';
 
@@ -26,8 +27,9 @@ export default Router.extend({
     edit(id) {
         NavbarService.setContentActive('hours');
         HeaderService.setTitle('Edit Hours');
+        let model = this.collection.get(id) || new HourModel({id: id});
 
-        return new ProtectedRoute(new EditView({model: this.collection.get(id)}));
+        return new ProtectedRoute(new EditView({model: model}));
     },
 });
 
