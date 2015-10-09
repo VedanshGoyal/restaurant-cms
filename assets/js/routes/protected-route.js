19d7cd5515e8fbd22efdb24c4816ec12e8ebdd56
Nick C <me@nickc.io>
2015-10-09 19:14:27 -0400

    update routes code style

--- assets/js/routes/protected-route.js
+++ assets/js/routes/protected-route.js
@@ -5,27 +5,16 @@ export default Route.extend({
     redirectHash: 'login',
 
     enter(rest = []) {
-        this._isEntering = true;
         this.trigger('before:enter before:fetch', this, ...rest);
 
         return Promise.resolve().then(() => {
             return this.fetch(...rest);
-        }).then(() => {
-            this.trigger('fetch before:render', this, ...rest);   
         }).then(() => {
             if (AuthService.isAuthed()) {
                 this.render(...rest);
             } else {
                 window.location.hash = this.redirectHash;
             }
-        }).then(() => {
-            this._isEntering = false;   
-            this.trigger('render enter', this, ...rest);
-        }).catch(error => {
-            this._isEntering = false;
-            this.trigger('error error:enter', this, ...rest);
-
-            throw error;
         });
     },
 });
