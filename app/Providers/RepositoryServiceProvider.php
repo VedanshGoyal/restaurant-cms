8fe1ee3635b154e7e9b3704e3e57958ebfc37dd1
Nick C <me@nickc.io>
2015-10-10 19:23:07 -0400

    remove unused paramter from repository service provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -35,63 +35,63 @@ class RepositoryServiceProvider extends ServiceProvider
 
     private function registerMenuSectionRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\MenuSectionRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\MenuSectionRepo', function () {
             return new \Restaurant\Repositories\MenuSectionRepo(new MenuSection());
         });
     }
 
     private function registerMenuItemRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\MenuItemRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\MenuItemRepo', function () {
             return new \Restaurant\Repositories\MenuItemRepo(new MenuItem());
         });
     }
 
     private function registerHourRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\HourRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\HourRepo', function () {
             return new \Restaurant\Repositories\HourRepo(new Hour());
         });
     }
 
     private function registerAboutRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\AboutRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\AboutRepo', function () {
             return new \Restaurant\Repositories\AboutRepo(new About());
         });
     }
 
     private function registerInfoRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\InfoRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\InfoRepo', function () {
             return new \Restaurant\Repositories\InfoRepo(new Info());
         });
     }
 
     private function registerPhotoRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\PhotoRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\PhotoRepo', function () {
             return new \Restaurant\Repositories\PhotoRepo(new Photo());
         });
     }
 
     private function registerSiteConfigRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\SiteConfigRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\SiteConfigRepo', function () {
             return new \Restaurant\Repositories\SiteConfigRepo(new SiteConfig());
         });
     }
 
     public function registerUsersRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\UsersRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\UsersRepo', function () {
             return new \Restaurant\Repositories\UsersRepo(new User());
         });
     }
 
     public function registerRolesRepo()
     {
-        $this->app->singleton('Restaurant\Repositories\RolesRepo', function ($app) {
+        $this->app->singleton('Restaurant\Repositories\RolesRepo', function () {
             return new \Restaurant\Repositories\RolesRepo(new Role());
         });
     }
