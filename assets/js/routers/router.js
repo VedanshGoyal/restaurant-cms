573398a12755c0a959c11b85d756cb775f7fece9
Nick C <me@nickc.io>
2015-10-19 23:08:34 -0400

    fix base router to pass callback arguments as spread

--- assets/js/routers/router.js
+++ assets/js/routers/router.js
@@ -2,7 +2,7 @@ import {Router} from 'backbone';
 
 export default Router.extend({
     execute(callback, args) {
-        const route = callback.call(this, args);
+        const route = callback.call(this, ...args);
 
         route.enter();
     },
