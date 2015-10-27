81da1c4ef646643c700061c4b4e3eca68460b5f4
Nick C <me@nickc.io>
2015-10-26 20:28:02 -0400

    add defaults for menu section infoTitle and info values

--- assets/js/models/menu-section.js
+++ assets/js/models/menu-section.js
@@ -3,8 +3,10 @@ import Model from './model';
 export default Model.extend({
     resourceName: 'menu-section',
     defaults: {
-        itemPrices: 1,
+        sizes: 1,
         sortId: 1,
         name: '',
+        infoTitle: '',
+        info: '',
     },
 });
