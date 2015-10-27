9efdece9ab4d23f37a2ecc39cde5abe5250d2cd9
Nick C <me@nickc.io>
2015-10-26 20:26:38 -0400

    fix typo in menu section request validation rules

--- app/Http/Requests/MenuSectionRequest.php
+++ app/Http/Requests/MenuSectionRequest.php
@@ -8,7 +8,7 @@ class MenuSectionRequest extends Request
 {
     // @var array
     protected $rules = [
-        'sortId' => 'required|integer|beteween:1,1000',
+        'sortId' => 'required|integer|between:1,1000',
         'itemPrices' => 'required|integer|between:1,2',
         'name' => 'required|basicText|between:3,150',
         'infoTitle' => 'basicText|between:3,200',
