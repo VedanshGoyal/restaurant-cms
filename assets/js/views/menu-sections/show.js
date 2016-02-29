c555a28d57f3d9f1a0ef9229e08ce6e94e35834f
Nick C <me@nickc.io>
2016-02-28 23:42:19 -0500

    add functionlaity to delete menu sections

--- assets/js/views/menu-sections/show.js
+++ assets/js/views/menu-sections/show.js
@@ -1,5 +1,8 @@
 import {ItemView, CompositeView, TemplateCache} from 'backbone.marionette';
 import MDLBehavior from '../../behaviors/mdl';
+import DialogService from '../../services/dialog';
+import LoadingService from '../../services/loading';
+import NotifyService from '../../services/notify';
 
 const MenuItemView = ItemView.extend({
     template: TemplateCache.get('itemShowTable'),
@@ -21,12 +24,39 @@ export default CompositeView.extend({
         sync: 'render',
     },
 
+    events: {
+        'click .delete': 'deleteDialog',
+    },
+
     initialize() {
         this.model.fetch();
     },
 
     onBeforeRender() {
         this.collection.add(this.model.get('items'), {silent: true});
+    },
+
+    deleteDialog() {
+        const title = `Delete Section`;
+        const message = `Deleting ${this.model.get('name')} will also remove its ${this.model.get('items').length} items`;
+
+        DialogService.open(title, message)
+        .then(() => this.confirmDelete())
+        .catch(() => DialogService.close());
+    },
+
+    confirmDelete() {
+        DialogService.close();
+        LoadingService.show();
+
+        this.model.destroy().done(() => {
+            LoadingService.hide();
+            NotifyService.success('Menu section deleted');
+            window.location.hash = 'menu-sections';
+        }).fail(() => {
+            LoadingService.hide();
+            NotifyService.error('Failed to delete menu section');
+        });
     },
 });
 
