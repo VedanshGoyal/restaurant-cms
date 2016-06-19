3160647dc795eda3cc33aca5a04295e3bdc6b675
Nick C <me@nickc.io>
2016-06-19 13:42:13 -0400

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,81 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+// HTML routes
+Route::get('/', 'HTMLController@home');
+Route::get('/dash', 'HTMLController@dash');
 
-Route::get('/', function () {
-    return view('welcome');
+// API routes
+Route::group(['prefix' => 'api-V1'], function () {
+
+    // Auth routes
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/reset-password', 'AuthController@resetPassword');
+    Route::post('/verify-new', 'AuthController@verifyNew');
+    Route::post('/verify-reset', 'AuthController@verifyReset');
+
+    // Admin only routes
+    Route::group(['middleware' => ['token']], function () {
+
+        Route::resource(
+            'site-config',
+            'SiteConfigController',
+            ['only' => ['show', 'update']]
+        );
+
+        Route::resource(
+            'role',
+            'RolesController',
+            ['only' => ['index']]
+        );
+
+        Route::resource(
+            'user',
+            'UsersController',
+            ['only' => ['index']]
+        );
+
+    });
+
+    // Owner only routes
+    Route::group(['middleware' => ['token']], function () {
+
+        Route::resource(
+            'menu-section',
+            'MenuSectionsController',
+            ['only' => ['index', 'store', 'show', 'update', 'destroy']]
+        );
+
+        Route::resource(
+            'menu-item',
+            'MenuItemsController',
+            ['only' => ['store', 'show', 'update', 'destroy']]
+        );
+
+        Route::resource(
+            'about',
+            'AboutController',
+            ['only' => ['show', 'update']]
+        );
+
+        Route::resource(
+            'info',
+            'InfoController',
+            ['only' => ['show', 'update']]
+        );
+
+        Route::resource(
+            'hour',
+            'HoursController',
+            ['only' => ['index', 'show', 'update']]
+        );
+
+        Route::resource(
+            'photo',
+            'PhotosController',
+            ['only' => ['index', 'store', 'destroy']]
+        );
+
+    });
+
 });
