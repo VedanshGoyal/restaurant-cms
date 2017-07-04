76da8d94e63f98525e48593b2ab83ed574d8f50d
Nick C <me@nickc.io>
2017-07-03 23:45:55 -0400

    Add missing photo and site config model code

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace RestaurantCMS;
+namespace RestaurantCMS\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Sofa\Eloquence\Eloquence;
+use Sofa\Eloquence\Mappable;
 
 class Photo extends Model
 {
-    //
+    use Eloquence, Mappable;
+
+    // @var string - DB table name
+    protected $table = 'photos';
+
+    // @var array<string> - Mass-assignable fields
+    protected $fillable = ['sortId', 'path'];
+
+    // @var array - Property name maps
+    protected $maps = [
+        'sortId' => 'sort_id',
+        'createdAt' => 'created_at',
+        'updatedAt' => 'updated_at',
+    ];
+
+    // @var array<string> - Fields to be hidden when doing array/json serialization
+    protected $hidden = ['sort_id', 'created_at', 'updated_at'];
+
+    // @var array<string> - Fields to be added when doing array/json serialization
+    protected $appends = ['sortId', 'createdAt', 'updatedAt'];
 }
--- app/Models/SiteConfig.php
+++ app/Models/SiteConfig.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace RestaurantCMS;
+namespace RestaurantCMS\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Sofa\Eloquence\Eloquence;
+use Sofa\Eloquence\Mappable;
 
 class SiteConfig extends Model
 {
-    //
+    use Eloquence, Mappable;
+
+    // @var string - DB table name
+    protected $table = 'site_config';
+
+    // @var array - mass-assignable properties
+    protected $fillable = ['allow_reg'];
+
+    // @var array - Property name maps
+    protected $maps = ['allowReg' => 'allow_reg', 'updatedAt' => 'updated_at'];
+
+    // @var array<string> - Fields to be hidden when doing array/json serialization
+    protected $hidden = ['allow_reg', 'updated_at'];
+
+    // @var array<string> - Fields to be added when doing array/json serialization
+    protected $appends = ['allowReg', 'updatedAt'];
 }
