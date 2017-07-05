f859ca386cce0fb67a10181ff4525e4cb3e075a8
Nick C <me@nickc.io>
2017-07-04 23:26:01 -0400

    Setup test tools
    - Add mockery
    - Add basic test helper methods to TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Mockery as m;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getMockCollection()
+    {
+        return m::mock('Illuminate\Database\Eloquent\Collection')->makePartial();
+    }
+
+    protected function getMockModel()
+    {
+        return m::mock('Illuminate\Database\Eloquent\Model')->makePartial();
+    }
 }
